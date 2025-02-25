6f47db2aa6b6645ec3c86f918a1f5bb57ad18256
Madina <mmedikoshh@gmail.com>
2025-02-25 16:12:21 +0500

    initial commit

---
+++ FINAL/final.py
@@ -0,0 +1,607 @@
+1
+Not
+yet
+answered
+Marked
+out
+of
+12.00
+Flag
+question
+Question
+text
+Suppose
+that
+you
+have
+the
+following
+results
+of
+survey.Now
+it is necessary
+to
+analyze
+obtained
+survey
+by
+using
+Simple
+Random
+Sampling and Stratified
+Random
+Sampling.Recommended
+tool
+for using is MS Excel.
+
+(Ignore the fpc and the clustering in calculating the standard error.)
+
+P.S.when
+you
+are
+going
+to
+write
+your
+answer
+into
+answer
+sheet, please
+round
+up
+to
+2
+digits
+after
+floating
+point
+
+1) Assume
+that
+you
+'re going to do Simple Random Sampling (SRS) for above dataset.
+
+Compute
+a
+mean: blank
+blank
+blank
+blank
+
+P.S
+here
+you
+need
+to
+drag - and -drop
+digit - by - digit.For
+example,
+if your answer is 35.13 then you need to drag 3, then 5, then.and then 1 and 3
+
+2) Compute a standard error for SRS:  blank
+blank
+blank
+blank
+
+3) Now
+compute
+95 % of
+confidence
+interval.Please
+note
+that
+t - value in this
+case is equal
+to
+2.04
+
+Upper
+limit
+for SRS:  blank
+blank
+blank
+blank
+blank
+
+Lower
+limit
+for SRS:  blank
+blank
+blank
+blank
+blank
+
+P.S
+You
+need
+to
+take
+rounded
+answers
+from
+
+1
+st and 2
+nd
+questions
+
+Now
+for Stratified part
+
+1) What is the value of Wh        blank  blank  blank  blank
+
+2) Compute a mean:  blank
+blank
+blank
+blank
+
+3) Compute
+a
+standard
+error
+for Stratified part:  blank
+blank
+blank
+
+4) Compute
+d - value: blank
+blank
+blank
+blank
+Hint: d - value is a
+ratio
+of
+standard
+error
+for statified over standard error for SRS
+5) Compute d-squared:  blank
+blank
+blank
+blank
+6) Compute
+Neff
+blank
+blank
+blank
+blank
+P.S
+you
+need
+to
+take
+exact
+anwer
+from
+
+5 - th
+step
+rounded
+up
+to
+2
+digits
+after
+floating
+point.
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0,
+Question
+2
+Not
+yet
+answered
+Marked
+out
+of
+10.00
+Flag
+question
+Question
+text
+Suppose
+that
+you
+'re going to run linear regression with 4 input features and 1 output feature. Your hypothesis is linear.
+
+Firstly
+it
+it
+necessary
+to
+normalize
+your
+dataset: Z = (x - mu) / std
+
+Initial
+theta
+parameters is equal
+to
+zero.Learning
+rate is 0.01.Now, let
+'s complete the following table:
+
+# Iterations
+
+Cost
+Function(Round
+please
+up
+to
+integer
+value)
+
+Optimal
+Theta
+parameter
+
+Indicate
+here
+maximum
+theta
+value(round
+please
+up
+to
+2
+digits
+after
+floating
+point)
+
+n = 10
+
+blank
+blank
+blank
+
+blank
+blank
+blank
+blank
+
+n = 100
+
+blank
+blank
+
+blank
+blank
+blank
+blank
+blank
+
+n = 1000
+
+blank
+
+blank
+blank
+blank
+blank
+blank
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0,
+Question
+3
+Not
+yet
+answered
+Marked
+out
+of
+10.00
+Flag
+question
+Question
+text
+Suppose
+that
+you
+'re going to run logistic regression with 3 input features and 1 output feature. Your hypothesis is linear with sigmoid activation function.
+
+Initial
+theta
+parameters is equal
+to
+zero.Learning
+rate is 0.01.Now, let
+'s complete the following table:
+
+# Iterations
+
+Cost
+Function(Round
+up
+to
+3
+digits
+after
+floating
+point)
+
+Optimal
+Theta
+parameter
+
+Indicate
+here
+maximum
+theta
+value(Round
+up
+to
+2
+digits
+after
+floating
+point)
+
+n = 10
+
+blank
+blank
+blank
+blank
+blank
+blank
+
+blank
+blank
+blank
+blank
+
+n = 100
+
+blank
+blank
+blank
+blank
+blank
+
+blank
+blank
+blank
+blank
+
+n = 1000
+
+blank
+blank
+blank
+blank
+blank
+
+blank
+blank
+blank
+blank
+
+After
+10.000
+iterations and by
+setting
+threshold = 0.7, what is the
+number
+of
+ones in the
+first
+20
+rows
+of
+prediction:
+blank
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0,
+Question
+4
+Not
+yet
+answered
+Marked
+out
+of
+10.00
+Flag
+question
+Question
+text
+Suppose
+that
+you
+'re going to run neural network algorithm with the following parameters:
+
+Data
+Preparation: Normalized
+input
+vectors
+for binary classification.
+        Network Architecture: Three
+hidden
+layers
+with ReLU activation, and an output layer with Sigmoid activation.
+Forward Propagation: Computes
+activations
+through
+each
+layer.
+Loss
+Calculation: Uses
+Mean
+Absolute
+Error(MAE) as the
+loss
+function.
+Backpropagation: Computes
+gradients
+using
+the
+chain
+rule.
+Weight and Bias
+Updates: Updates
+parameters
+using
+gradient
+descent
+with a learning rate.
+Iterative Training: Runs
+for 10, 000 epochs, printing loss every 1, 000 epochs.
+Final Prediction: Outputs
+probability
+values
+for each input.
+        a4 =[blank  blank  blank, blank  blank  blank]
+
+a3.max() =  blank  blank  blank           (round up to  integer value)
+
+a2.max() =  blank  blank           (round up to  integer value)
+
+a1.max() =  blank           (round up to  integer value)
+
+Loss:  blank
+blank
+blank
+General
+Conculsion
+after
+10000
+epochs: blank
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0,
+NN
+predicts
+image
+of
+dog
+NN
+predicts
+image
+of
+cat
+NN
+can
+'t define correct image class
+Question
+5
+Not
+yet
+answered
+Marked
+out
+of
+9.00
+Flag
+question
+Question
+text
+You
+trained
+the
+neural
+network
+for 10 epochs and obtained the following confusion matrix on the test set:
+
+
+
+    Predicted: Cat(1)
+
+Predicted: Not
+Cat(0)
+
+Actual: Cat(1)
+
+200
+
+300
+
+Actual: Not
+Cat(0)
+
+120
+
+180
+
+Calculate
+the
+accuracy, precision, and recall
+of
+the
+classifier
+using
+the
+confusion
+matrix.
+
+Accuracy: blank
+blank
+blank
+blank
+blank
+Precision: blank
+blank
+blank
+blank
+blank
+Recall: blank
+blank
+blank
+F - 1
+Score: blank
+blank
+blank
+blank
+blank
+Please
+round
+up
+to
+3
+digits
+after
+floating
+point
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0,
